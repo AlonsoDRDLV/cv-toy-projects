cf9b0ece73253fd8b15c5a6b046758f20558423f
735184 <48834236+735184@users.noreply.github.com>
2022-03-22 11:25:03 +0100

    :cn: xdxd :anger:

--- P2/p2.cpp
+++ P2/p2.cpp
@@ -15,9 +15,10 @@ using std::cout;
 using std::endl;
 using namespace cv;
 
-int isWindowOpen(const cv::String& name);
-void openWindow(const cv::String & name);
-void closeWindow(const cv::String& name);
+Mat hough(Mat image, int minThreshold, int maxThreshold);
+Mat gradX(Mat gray_blurred_image);
+Mat gradY(Mat gray_blurred_image);
+Mat angles(Mat gX, Mat gY);
 Mat their_Sobel(Mat source);
 Mat our_Sobel(Mat source);
 Mat their_Scharr(Mat source);
@@ -28,7 +29,7 @@ Mat our_Canny(Mat source);
 
 int main(int argc, char** argv){
   Mat image, copy, altered_image, opencv_image;
-  std::string imageName = "C:\\Users\\pica\\Documents\\GitHub\\super-duper-system\\P2\\recanny.jpg";
+  std::string imageName = "C:\\Users\\pica\\Documents\\GitHub\\super-duper-system\\P2\\pasillo2.pgm";
 
   image = imread(samples::findFile(imageName), IMREAD_COLOR);
   if(image.empty()){
@@ -36,10 +37,14 @@ int main(int argc, char** argv){
     return EXIT_FAILURE;
   }
 
-  copy = image.clone();
+  altered_image = hough(image, 50, 200);
 
-  altered_image = their_Canny(image);
-  opencv_image = our_Canny(copy);
+  imshow("their", altered_image);
+
+
+  //copy = image.clone();
+  //altered_image = their_Canny(image);
+  //opencv_image = our_Canny(copy);
   
  /* imshow("orig", image);
   imshow("pruebas", altered_image);
@@ -50,19 +55,86 @@ int main(int argc, char** argv){
   return EXIT_SUCCESS;
 }
 
-int isWindowOpen(const cv::String& name){
-  return cv::getWindowProperty(name, cv::WND_PROP_AUTOSIZE) != -1;
+Mat hough(Mat image, int minThreshold, int maxThreshold){
+  Mat blurred_image;
+  GaussianBlur(image, blurred_image, Size(5, 5), 0, 0, BORDER_DEFAULT);
+  Mat gray_blurred_image;
+  cvtColor(blurred_image, gray_blurred_image, COLOR_BGR2GRAY);
+
+
+  Mat gX = gradX(gray_blurred_image);
+  Mat gY = gradY(gray_blurred_image);
+
+  Mat ang = angles(gX, gY);
+
+  Mat canny_result;
+  Canny(gray_blurred_image, canny_result, minThreshold, maxThreshold, 3);
+
+  Mat canny_coloured;
+  cvtColor(canny_result, canny_coloured, COLOR_GRAY2BGR);
+
+  std::vector<Vec2f> lines;
+  HoughLines(canny_result, lines, 1, CV_PI / 180, maxThreshold - minThreshold, 0, 0);
+  for (size_t i = 0; i < lines.size(); i++){
+    float rho = lines[i][0], theta = lines[i][1];
+    Point pt1, pt2;
+    double a = cos(theta), b = sin(theta);
+    double x0 = a * rho, y0 = b * rho;
+    pt1.x = cvRound(x0 + 1000 * (-b));
+    pt1.y = cvRound(y0 + 1000 * (a));
+    pt2.x = cvRound(x0 - 1000 * (-b));
+    pt2.y = cvRound(y0 - 1000 * (a));
+    line(canny_coloured, pt1, pt2, Scalar(0, 0, 255), 3, LINE_AA);
+  }
+  return canny_coloured;
+  
 }
 
-void openWindow(const cv::String& name){
-  cv::namedWindow(name);
-  cvui::watch(name);
+Mat gradX(Mat gray_blurred_image){
+  Mat source = gray_blurred_image.clone();
+  Mat cannyX, gradX;
+  Mat verticalK = (Mat_<float>(3, 3) <<
+    -1, 0, 1,
+    -2, 0, 2,
+    -1, 0, 1);
+
+  filter2D(source, cannyX, CV_32F, verticalK);
+
+  convertScaleAbs(cannyX, gradX);
+
+  return gradX;
 }
 
-void closeWindow(const cv::String& name){
-  cv::destroyWindow(name);
+Mat gradY(Mat gray_blurred_image){
+  Mat source = gray_blurred_image.clone();
+  Mat cannyY, gradY;
+  Mat horizontalK = (Mat_<float>(3, 3) <<
+    1, 2, 1,
+    0, 0, 0,
+    -1, -2, -1);
 
-  cv::waitKey(1);
+  filter2D(source, cannyY, CV_32F, horizontalK);
+
+  convertScaleAbs(cannyY, gradY);
+
+  return gradY;
+}
+
+Mat angles(Mat gX, Mat gY){
+  Mat gAuxX = gX.clone();
+  Mat gAuxY = gY.clone();
+
+  gAuxX.convertTo(gAuxX, CV_32F);
+  gAuxY.convertTo(gAuxY, CV_32F);
+  Mat ang = Mat(gAuxX.size(), CV_32F, 0.0);
+
+
+  for(int y = 0; y < gAuxX.rows; y++){
+    for(int x = 0; x < gAuxY.cols; x++){
+       ang.at<float>(y, x) = atan2(gAuxX.at<float>(y, x), gAuxY.at<float>(y, x));
+    }
+  }
+  return ang;
 }
 
 Mat their_Sobel(Mat source){
@@ -212,6 +284,7 @@ Mat our_Canny(Mat source){
 
   imshow("4", angles); // Tendría que imprimir las direcciones (mirar wikipedia de operador canny, sería la imagen 4)
 
+  canny_result.convertTo(canny_result, CV_32F);
   // Non-max supression
   Mat nonMaxSup = Mat(angles.size(), angles.type(), 0.0);
   for (int y = 0; y < angles.rows; y++){
@@ -223,47 +296,37 @@ Mat our_Canny(Mat source){
     }
   }
   float oneSide, theOtherSide;
-  for (int y = 0; y < angles.rows; y++){
+  for (int y = 1; y < angles.rows - 1; y++){
     //float* puntY = angles.ptr<float>(y);
-    for (int x = 0; x < angles.cols; x++){
+    for (int x = 1; x < angles.cols - 1; x++){
       oneSide = 255;
       theOtherSide = 255;
       float angle = angles.ptr<float>(y)[x];
       if (((angle >= 0) && (angle < 22.5)) || ((angle >= 157.5) && (angle <= 180))){
-        if ((x > 0) && (x + 1 < angles.cols)){
-          oneSide = canny_result.ptr<float>(y)[x + 1];
-          theOtherSide = canny_result.ptr<float>(y)[x - 1];
-        }
+        oneSide = canny_result.ptr<float>(y)[x + 1];
+        theOtherSide = canny_result.ptr<float>(y)[x - 1];
       }else if ((angle >= 22.5) && (angle < 67.5)){
-        if ((x > 0) && (x + 1 < angles.cols) && (y > 0) && (y + 1 < angles.rows)){
-          oneSide = canny_result.ptr<float>(y + 1)[x - 1];
-          theOtherSide = canny_result.ptr<float>(y - 1)[x + 1];
-        }
+        oneSide = canny_result.ptr<float>(y + 1)[x - 1];
+        theOtherSide = canny_result.ptr<float>(y - 1)[x + 1];
       }else if ((angle >= 67.5) && (angle < 112.5)){
-        if ((y > 0) && (y + 1 < angles.rows)){
-          oneSide = canny_result.ptr<float>(y + 1)[x];
-          theOtherSide = canny_result.ptr<float>(y - 1)[x];
-        }
+        oneSide = canny_result.ptr<float>(y + 1)[x];
+        theOtherSide = canny_result.ptr<float>(y - 1)[x];
       }else if ((angle >= 112.5) && (angle < 157.5)){
-        if ((x > 0) && (x + 1 < angles.cols) && (y > 0) && (y + 1 < angles.rows)){
-          oneSide = canny_result.ptr<float>(y - 1)[x - 1];
-          theOtherSide = canny_result.ptr<float>(y + 1)[x + 1];
-        }
+        oneSide = canny_result.ptr<float>(y - 1)[x - 1];
+        theOtherSide = canny_result.ptr<float>(y + 1)[x + 1];
       }
 
       float aux = canny_result.ptr<float>(y)[x];
       if ((aux >= oneSide) && (aux >= theOtherSide)){
         nonMaxSup.ptr<float>(y)[x] = aux;
-      }else{
-        nonMaxSup.ptr<float>(y)[x] = 0;
       }
     }
   }
+  //cout << nonMaxSup << endl;
+
+  nonMaxSup.convertTo(nonMaxSup, CV_8U);
 
   imshow("5", nonMaxSup);
 
-
-  altered_image = Scalar::all(0);
-  gray_blurred_source.copyTo(altered_image, canny_result);
   return altered_image;
 }
