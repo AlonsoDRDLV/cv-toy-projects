ba1876a1f5863207a9869481eb00e39099dbbdea
AlonsoDRDLV <alonsodrdlv@gmail.com>
2022-04-11 20:29:55 +0200

    halahuakbar con formula chachi. no debuggado pero compila

--- P3/reconocer.cpp
+++ P3/reconocer.cpp
@@ -18,7 +18,7 @@ using std::cout;
 using std::endl;
 using namespace cv;
 
-const string PATH = "C:\\Users\\pica\\Documents\\GitHub\\super-duper-system\\P3\\images\\";
+const string PATH = "C:\\Users\\AlonsoDRDLV\\Documents\\GitHub\\super-duper-system\\P3\\images\\";
 const string DATA_NAME = "objetos.txt";
 const int BUFF_LENGTH = 1024;
 const int NUM_DESCRIPTORS = 5;
@@ -36,8 +36,8 @@ Mat otsu_threshold(Mat image, int gauss_size);
 //<nombre_de_la_figura2>;...
 //...
 // Siendo 1) area 2) perimetro 3) momento1 4) momento2 5) momento3
-int main(int argc, char** argv){
-  if(argc != 2){ // Check parameters
+int main(int argc, char** argv) {
+  if (argc != 2) { // Check parameters
     cout << "Wrong number of parameters, usage:\n";
     cout << "reconocer <nomfich>\n";
     exit(1);
@@ -57,17 +57,17 @@ int main(int argc, char** argv){
 
   std::ifstream objects(PATH + DATA_NAME);
 
-  if(objects.is_open()){ // Existen datos anteriores
-    do{
+  if (objects.is_open()) { // Existen datos anteriores
+    do {
       objects.read(buffer, BUFF_LENGTH);
       readedCount = objects.gcount();
       buffer_s = buffer_s + string(buffer).substr(0, readedCount);
-    }while (readedCount == BUFF_LENGTH);
+    } while (readedCount == BUFF_LENGTH);
 
     // Los divide en lineas
     pos = buffer_s.find("\n");
-    while (pos != string::npos){
-      if (pos > MIN_LENGTH_LINE){
+    while (pos != string::npos) {
+      if (pos > MIN_LENGTH_LINE) {
         lines.push_back(buffer_s.substr(0, pos));
       }
       buffer_s.erase(0, pos + 1);
@@ -76,11 +76,11 @@ int main(int argc, char** argv){
     objects.close();
 
     // Indexa los nombres de las clases y sus datos
-    for (int i = 0; i < lines.size(); i++){
+    for (int i = 0; i < lines.size(); i++) {
       pos = lines[i].find(";");
       classes.push_back(lines[i].substr(0, pos));
       lines[i].erase(0, pos + 1);
-      for (int j = 1; j < NUM_FIELDS; j++){
+      for (int j = 1; j < NUM_FIELDS; j++) {
         pos = lines[i].find(";");
         aux.push_back(stod(lines[i].substr(0, pos)));
         lines[i].erase(0, pos + 1);
@@ -88,12 +88,14 @@ int main(int argc, char** argv){
       data.push_back(aux);
     }
 
-  }else{ // Primer objeto aprendido
+  }
+  else { // Primer objeto aprendido
     cout << "No encuentra objetos.txt, no se puede reconocer nada\n";
     exit(1);
   }
 
-  // Lee el archivo a aprender
+  
+  // Lee el archivo a clasificar
   Mat image = imread(samples::findFile(fich_name), IMREAD_COLOR);
   if(image.empty()){
     printf("Error opening image: %s\n", fich_name.c_str());
@@ -111,6 +113,56 @@ int main(int argc, char** argv){
   findContours(canny, contours, RETR_TREE, CHAIN_APPROX_SIMPLE);
 
   // Saca los valores de los descriptores de todos los objetos detectados
+  vector<vector<double>> descriptores_objetos; //area, perimetro, momentos
+  vector<double> v_aux;
+  for (int i = 0; i < contours.size(); i++){
+    v_aux.push_back(contourArea(contours[i]));
+    v_aux.push_back(arcLength(contours[i], true));
+    Moments mu = moments(contours[i]);  
+    double huMoments[3] = {0.0, 0.0, 0.0};
+    for(int moment = 0; moment < 3; moment++){
+      huMoments[moment] = -1 * copysign(1.0, huMoments[moment]) 
+          * log10(abs(huMoments[moment]));
+    }
+    v_aux.push_back(huMoments[0]);
+    v_aux.push_back(huMoments[1]);
+    v_aux.push_back(huMoments[2]);
+    
+    descriptores_objetos.push_back(v_aux);
+    
+    v_aux.clear();
+  }
+
+  //cada componente: {Distancia del objeto i a la clase j}
+  vector<vector<double>> mahalanobis;
+  for (int i = 0; i < descriptores_objetos.size(); i++) {
+    vector<double> aux_v;
+    for (int clase = 0; clase < data.size(); clase++) {
+      double aux = 0.0;
+      int i_descriptor_clases = 0;
+      int i_descriptor_objetos = 0;
+      while (i_descriptor_clases < data[0].size()) {
+        double mean;
+        double variance;
+        double descriptor;
+        //int n = data[descriptor][0]; ???
+        descriptor = descriptores_objetos[i][i_descriptor_objetos];
+        mean = data[clase][i_descriptor_clases];// *n;
+        variance = data[clase][i_descriptor_clases+1];// *n;
+        aux += pow((descriptor - mean), 2) / variance;
+
+        i_descriptor_clases += 2;
+        i_descriptor_objetos++;
+       
+      }
+      aux_v.push_back(aux);
+    }
+    mahalanobis.push_back(aux_v);
+    aux_v.clear();
+  }
+  
+  /*
+   // Saca los valores de los descriptores de todos los objetos detectados
   vector<float> areas;
   vector<float> perims;
   vector<double> firstHuMoments;
@@ -130,12 +182,11 @@ int main(int argc, char** argv){
     thirdHuMoments.push_back(huMoments[2]);
   }
 
-  // Covars matrixes
-  vector<Mat> covars;
+  vector<double> mahalanobis;
   for (int classes = 0; classes < data.size(); classes++){
-    Mat covar = Mat_<double>(5, 5);
+    double means[5];
     double variances[5];
-    int n = data[classes][0];
+    int n = data[classes][0]; //POR QUE * N?
     variances[0] = data[classes][2] * n;
     variances[1] = data[classes][4] * n;
     variances[2] = data[classes][6] * n;
@@ -148,7 +199,7 @@ int main(int argc, char** argv){
     }
     covars.push_back(covar);
   }
-
+  
   // Mahalanobis
   double array1;
   double array2;
@@ -159,7 +210,7 @@ int main(int argc, char** argv){
       distances.push_back(Mahalanobis(array1, array2, matrix));
     }
   }
-
+  */
 
   waitKey(0);
   
